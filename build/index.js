876d7a9767cc0a0e9f6d22e05f2a7538be7db497
Blessing U <blessing986@users.noreply.github.com>
2024-04-19 12:01:36 +0100

    chore: refactord code.

--- build/index.js
+++ build/index.js
@@ -1,20 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
 const LinkedLists_1 = require("./LinkedLists");
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 // const charactersCollection = new CharactersCollection("Xaayb");
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort()
 // console.log(charactersCollection.data);
 const linkedList = new LinkedLists_1.LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-const sorter = new Sorter_1.Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
